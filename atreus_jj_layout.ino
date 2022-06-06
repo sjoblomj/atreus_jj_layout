3e4aa2325f755f175db2cfe1f3ecde97ea7ff279
Johan Sjöblom <sjoblomj88@gmail.com>
2022-06-06 18:07:59 +0200

    Adding firmware, Layout Creator and readme

---
+++ atreus_jj_layout.ino
@@ -0,0 +1,180 @@
+/* -*- mode: c++ -*-
+   Atreus -- Chrysalis-enabled Sketch for the Keyboardio Atreus
+   Copyright (C) 2018, 2019  Keyboard.io, Inc
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License along
+   with this program; if not, write to the Free Software Foundation, Inc.,
+   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+#include "Kaleidoscope.h"
+#include "Kaleidoscope-MouseKeys.h"
+#include "Kaleidoscope-ShapeShifter.h"
+
+
+enum {
+  COLEMAK,
+  FUN,
+  UPPER,
+  MOVE
+};
+
+/* *INDENT-OFF* */
+/* *LAYOUTER-START* */
+#define MO(n) ShiftToLayer(n)
+#define TG(n) LockLayer(n)
+
+// Key definitions at https://github.com/keyboardio/Kaleidoscope/blob/master/src/kaleidoscope/key_defs/keyboard.h
+#define Key_Exclamation        LSHIFT(Key_1)                     // !
+#define Key_DoubleQuote        LSHIFT(Key_2)                     // "
+#define Key_At                 RALT(Key_2)                       // @
+#define Key_Hash               LSHIFT(Key_3)                     // #
+#define Key_Dollar             RALT(Key_4)                       // $
+#define Key_Percent            LSHIFT(Key_5)                     // %
+#define Key_And                LSHIFT(Key_6)                     // &
+#define Key_SlashForward       LSHIFT(Key_7)                     // /
+#define Key_LeftCurlyBracket   RALT(Key_7)                       // {
+#define Key_LeftRoundBracket   LSHIFT(Key_8)                     // (
+#define Key_LeftSquareBracket  RALT(Key_8)                       // [
+#define Key_RightSquareBracket RALT(Key_9)                       // ]
+#define Key_RightRoundBracket  LSHIFT(Key_9)                     // )
+#define Key_RightCurlyBracket  RALT(Key_0)                       // }
+#define Key_Equals             LSHIFT(Key_0)                     // =
+#define Key_Question           LSHIFT(Key_Minus)                 // ? Shifted !
+#define Key_SlashBackward      RALT(Key_Minus)                   // Backslash
+#define Key_PlusSign           Key_Minus                         // +
+#define Key_RightSingleQuote   RALT(Key_Backslash)               // ´
+#define Key_Caret              LSHIFT(Key_RightBracket)          // ^
+#define Key_Tilde              RALT(Key_RightBracket)            // ~
+
+#define Key_Star               LSHIFT(Key_Backslash)             // *
+#define Key_SingleQuote        Key_Backslash                     // '
+#define Key_Aa                 Key_LeftBracket                   // Å
+#define Key_Ae                 Key_Quote                         // Ä
+#define Key_Oe                 Key_Semicolon                     // Ö
+#define Key_Nae                RALT(Key_Quote)                   // Æ
+#define Key_Noe                RALT(Key_Semicolon)               // Ø
+#define Key_MinusSign          Key_Slash                         // -
+#define Key_Underscore         LSHIFT(Key_Slash)                 // _
+#define Key_SectionSign        Key_Backtick                      // §
+#define Key_OneOverTwo         LSHIFT(Key_Backtick)              // ½
+#define Key_LeftAngleBracket   Key_NonUsBackslashAndPipe         // <
+#define Key_RightAngleBracket  LSHIFT(Key_NonUsBackslashAndPipe) // >
+#define Key_Pipe               RALT(Key_NonUsBackslashAndPipe)   // |
+#define Key_Euro               RALT(Key_E)                       // €
+#define Key_PeriodSign         Key_Period                        // . Shifted :
+#define Key_CommaSign          Key_Comma                         // , Shifted ;
+
+#define Key_Bksp               Key_Backspace                     // Bksp
+#define Key_Ctrl               Key_LeftControl                   // Ctrl
+#define Key_Shift              Key_LeftShift                     // Shift
+#define Key_Super              Key_LeftGui                       // Super
+#define Key_Alt                Key_LeftAlt                       // Alt
+#define Key_Ins                Key_Insert                        // Ins
+#define Key_Del                Key_Delete                        // Del
+#define Key_PgUp               Key_PageUp                        // Pg↑
+#define Key_PgDn               Key_PageDown                      // Pg↓
+#define Key_PrtSc              Key_PrintScreen                   // PrtSc
+#define Key_ArrowUp            Key_UpArrow                       // ↑
+#define Key_ArrowDown          Key_DownArrow                     // ↓
+#define Key_ArrowLeft          Key_LeftArrow                     // ←
+#define Key_ArrowRight         Key_RightArrow                    // →
+#define Key_MouseUp            Key_mouseUp                       // M↑
+#define Key_MouseDown          Key_mouseDn                       // M↓
+#define Key_MouseLeft          Key_mouseL                        // M←
+#define Key_MouseRight         Key_mouseR                        // M→
+#define Key_MouseClickL        Key_mouseBtnL                     // McL
+#define Key_MouseClickM        Key_mouseBtnM                     // McM
+#define Key_MouseClickR        Key_mouseBtnR                     // McR
+
+KEYMAPS(
+  [COLEMAK] = KEYMAP_STACKED
+  (
+                              Key_Q                ,Key_W                 ,Key_F                ,Key_P                 ,Key_G
+                             ,Key_A                ,Key_R                 ,Key_S                ,Key_T                 ,Key_D
+                             ,Key_Z                ,Key_X                 ,Key_C                ,Key_V                 ,Key_B                 ,Key_And
+                             ,Key_Esc              ,Key_Tab               ,Key_Super            ,Key_Shift             ,Key_Bksp              ,Key_Ctrl
+
+                             ,Key_J                ,Key_L                 ,Key_U                ,Key_Y                 ,Key_Oe
+                             ,Key_H                ,Key_N                 ,Key_E                ,Key_I                 ,Key_O
+      ,Key_Tilde             ,Key_K                ,Key_M                 ,Key_PeriodSign       ,Key_Aa                ,Key_Ae
+      ,Key_Alt               ,Key_Space            ,MO(FUN)               ,Key_CommaSign        ,Key_Question          ,Key_Enter
+  ),
+
+  [FUN] = KEYMAP_STACKED
+  (
+                              Key_SlashForward     ,Key_At                ,Key_ArrowUp          ,___                   ,Key_Dollar
+                             ,Key_DoubleQuote      ,Key_ArrowLeft         ,Key_ArrowDown        ,Key_ArrowRight        ,Key_Hash
+                             ,Key_LeftRoundBracket ,Key_RightRoundBracket ,Key_LeftCurlyBracket ,Key_RightCurlyBracket ,Key_LeftSquareBracket ,Key_RightSquareBracket
+                             ,TG(UPPER)            ,___                   ,___                  ,___                   ,Key_Del               ,___
+
+                             ,Key_PgUp             ,Key_7                 ,Key_8                ,Key_9                 ,Key_Equals
+                             ,Key_PgDn             ,Key_4                 ,Key_5                ,Key_6                 ,Key_Star
+      ,Key_LeftAngleBracket  ,Key_OneOverTwo       ,Key_1                 ,Key_2                ,Key_3                 ,Key_PlusSign
+      ,___                   ,TG(MOVE)             ,___                   ,___                  ,Key_0                 ,Key_MinusSign
+  ),
+
+  [UPPER] = KEYMAP_STACKED
+  (
+                              Key_SlashBackward    ,Key_MouseClickL       ,Key_MouseUp          ,Key_MouseClickM       ,Key_MouseClickR
+                             ,Key_SingleQuote      ,Key_MouseLeft         ,Key_MouseDown        ,Key_MouseRight        ,Key_Percent
+                             ,Key_RightSingleQuote ,Key_Euro              ,Key_Nae              ,Key_Noe               ,Key_Caret             ,Key_Pipe
+                             ,MoveToLayer(COLEMAK) ,___                   ,___                  ,___                   ,Key_Ins               ,___
+
+                             ,Key_Home             ,Key_F7                ,Key_F8               ,Key_F9                ,Key_F10
+                             ,Key_End              ,Key_F4                ,Key_F5               ,Key_F6                ,Key_F11
+      ,Key_RightAngleBracket ,Key_SectionSign      ,Key_F1                ,Key_F2               ,Key_F3                ,Key_F12
+      ,___                   ,___                  ,MoveToLayer(COLEMAK)  ,Key_PrtSc            ,___                   ,Key_Underscore
+  ),
+
+  [MOVE] = KEYMAP_STACKED
+  (
+                              ___                  ,Key_MouseClickL       ,Key_MouseUp          ,Key_MouseClickM       ,Key_MouseClickR
+                             ,___                  ,Key_MouseLeft         ,Key_MouseDown        ,Key_MouseRight        ,___
+                             ,___                  ,___                   ,___                  ,___                   ,___                   ,___
+                             ,MoveToLayer(COLEMAK) ,___                   ,___                  ,___                   ,___                   ,___
+
+                             ,Key_PgUp             ,___                   ,Key_ArrowUp          ,___                   ,Key_Home
+                             ,Key_PgDn             ,Key_ArrowLeft         ,Key_ArrowDown        ,Key_ArrowRight        ,Key_End
+      ,___                   ,___                  ,___                   ,___                  ,___                   ,___
+      ,___                   ,___                  ,MoveToLayer(COLEMAK)  ,___                  ,___                   ,___
+  ),
+)
+/* *LAYOUTER-STOP* */
+/* *INDENT-ON* */
+
+KALEIDOSCOPE_INIT_PLUGINS(
+  MouseKeys,
+  ShapeShifter
+);
+
+
+static const kaleidoscope::plugin::ShapeShifter::dictionary_t shape_shifter_dictionary[] PROGMEM = {
+  {Key_Question, Key_1}, // Replace Shift+'?' with Shift+'1' => '!'
+  {Key_NoKey, Key_NoKey},
+};
+
+void setup() {
+  Kaleidoscope.setup();
+
+  MouseKeys.setSpeedLimit(25);
+  MouseKeys.speed = 2;
+  MouseKeys.accelSpeed = 5;
+  MouseKeys.accelDelay = 150;
+
+  ShapeShifter.dictionary = shape_shifter_dictionary;
+}
+
+void loop() {
+  Kaleidoscope.loop();
+}
---
+++ layout_creator.py
@@ -0,0 +1,205 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+import re
+
+KEYBOARD_ROWS = 4
+SPECIAL_SYMBOLS = {
+    "Backslash": "\\",
+    "&": "&amp;",
+    "<": "&lt;",
+    ">": "&gt;"
+}
+
+# Constants relating to where the texts of different layers will appear, and to key positioning
+key_pos_offset = [100, 50, 0, 50, 100, 50, 50, 100, 50, 0, 50, 100]
+colours = {"COLEMAK": "black", "UPPER": "red", "MOVE": "green", "FUN": "blue"}
+anchors = {"COLEMAK": "start", "UPPER": "start", "MOVE": "middle", "FUN": "end"}
+font_size = {"COLEMAK": 35, "UPPER": 25, "MOVE": 25, "FUN": 25}
+x_positions = {"COLEMAK": 250, "UPPER": 240, "MOVE": 300, "FUN": 360}
+y_positions = {"COLEMAK": 160, "UPPER": 71, "MOVE": 100, "FUN": 71}
+key_size = 175
+x_key_rectangle_offset = 225
+y_key_rectangle_offset = 25
+shift_position_offset = -40
+
+definition_dict = {}
+shifted_dict = {}
+
+
+def read_file(file_name):
+    with open(file_name) as infile:
+        return infile.read().splitlines()
+
+
+def get_key_representation(key):
+    returned_key = None
+    if key == None or key == "___":
+        returned_key = None
+    elif key in definition_dict:
+        returned_key = definition_dict[key]
+    else:
+        returned_key = key.replace("Key_", "")
+    if returned_key in SPECIAL_SYMBOLS:
+        returned_key = SPECIAL_SYMBOLS[returned_key]
+    return returned_key
+
+
+def read_keylayout(keymaps):
+    layouts = {}
+    current_keyboard_row = 1
+    keymap_name = None
+    left_half   = None
+    right_half  = None
+
+    for line in keymaps:
+        res = re.search(r" *\[(.+)\] *= *KEYMAP_STACKED", line)
+        if res != None:
+            if keymap_name != None:
+                layouts[keymap_name] = [left_half, right_half]
+            keymap_name = res.group(1)
+            current_keyboard_row = 1
+            left_half  = [[], [], [], [], [], []]
+            right_half = [[], [], [], [], [], []]
+
+        key_name_chars = "([0-9a-zA-Z_\-()]+)"
+        res = re.search(r" *,? *" + key_name_chars + " *, *" + key_name_chars + " *, *" + key_name_chars + " *, *" + key_name_chars + " *, *" + key_name_chars + " *,? *" + key_name_chars + "?", line)
+
+        if res != None:
+            keyrow = res.groups()
+            start_index = 0
+            if current_keyboard_row == KEYBOARD_ROWS + 1 or current_keyboard_row == KEYBOARD_ROWS + 2:
+                start_index = -1
+
+            for index in range(0, len(keyrow)):
+                k = keyrow[(start_index + index) % len(keyrow)]
+                if current_keyboard_row <= KEYBOARD_ROWS:
+                    left_half[index].append(k)
+                else:
+                    right_half[index].append(k)
+
+            current_keyboard_row += 1
+
+    layouts[keymap_name] = [left_half, right_half]
+
+    output = []
+    for i in range(0, 12):
+        output.append([])
+        for j in range(0, 4):
+            output[i].append([])
+
+    for layout in layouts:
+        col_num = 0
+        for half in layouts[layout]:
+            for col in half:
+                row_num = 0
+                for key in col:
+                    if output[col_num][row_num] == []:
+                        output[col_num][row_num] = {layout: key}
+                    else:
+                        output[col_num][row_num].update({layout: key})
+                    row_num += 1
+                col_num += 1
+    return output
+
+def read_definitions(line):
+    res = re.search(r"#define ([^ ]*) .*// ?(.*)", line)
+    if res != None:
+        shiftMatch = re.search(r"([^ ]+) +[sS][hH][iI][fF][tT][eE][dD] +([^ ]+)", res.group(2))
+        if shiftMatch != None:
+            definition_dict[res.group(1)] = shiftMatch.group(1)
+            shifted_dict[res.group(1)] = shiftMatch.group(2)
+        else:
+            definition_dict[res.group(1)] = res.group(2)
+
+def create_svg_key(x_index, y_index, key_data):
+
+    xpos = x_index * key_size
+    ypos = y_index * key_size + key_pos_offset[x_index]
+
+    shifted = None
+    if key_data["COLEMAK"] in shifted_dict:
+        shifted = shifted_dict[key_data["COLEMAK"]]
+
+    output = "    <rect x=\"" + str(xpos + x_key_rectangle_offset) + "\" y=\"" + str(ypos + y_key_rectangle_offset) + "\" rx=\"20\" ry=\"20\" width=\"150\" height=\"150\" style=\"fill:white;fill-opacity:0;stroke:black;stroke-width:3;stroke-opacity:0.71\" />\n"
+    if shifted != None:
+        output += "    <text x=\"" + str(xpos + x_positions["COLEMAK"]) + "\" y=\"" + str(ypos + y_positions["COLEMAK"] + shift_position_offset) + "\" font-size=\"" + str(font_size["COLEMAK"]) + "\" text-anchor=\"" + anchors["COLEMAK"] + "\" fill=\"" + colours["COLEMAK"] + "\">" + shifted + "</text>\n"
+
+    for layer in list(key_data.keys()):
+        key = get_key_representation(key_data[layer])
+        if key != None:
+            res = re.search(r"MoveToLayer\(.*\)", key)
+            if res != None:
+                continue
+
+            # Colour the Layer names in their own colour, rather than that of the layer they are on.
+            colour = colours[layer]
+            res = re.search(r"(TG|MO)\((.*)\)", key)
+            if res != None:
+                key = res[2]
+                colour = colours[key]
+
+            output += "    <text x=\"" + str(xpos + x_positions[layer]) + "\" y=\"" + str(ypos + y_positions[layer]) + "\" font-family=\"Arial\" font-size=\"" + str(font_size[layer]) + "\" text-anchor=\"" + anchors[layer] + "\" fill=\"" + colour + "\">" + key + "</text>\n"
+
+    output += "\n"
+    return output
+
+def create_svg(layouts, outfile):
+    with open(outfile, "w") as f:
+        f.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n")
+        f.write("<svg xmlns=\"http://www.w3.org/2000/svg\"\n     xmlns:xlink=\"http://www.w3.org/1999/xlink\"\n     xmlns:ev=\"http://www.w3.org/2001/xml-events\"\n     width=\"2400\" height=\"1150\">\n")
+        f.write("  <polyline points=\"185,20 2100,20 2290,715 1450,950 830,950 5,715 185,20\" style=\"fill:none;stroke:black;stroke-width:3;stroke-opacity:0.71\" />\n")
+
+        col_num = 0
+        row_num = 0
+        for col in range(0, 12):
+            if col == 0:
+                f.write("  <g id=\"left\" transform=\"rotate(15, 500, 0)\">\n")
+            elif col == 6:
+                f.write("  </g>\n\n")
+                f.write("  <g id=\"right\" transform=\"rotate(-15, 1900, 900)\">\n")
+            for key_data in layouts[col]:
+                if (col_num == 5 or col_num == 6) and (row_num == 0 or row_num == 1):
+                    row_num += 1
+                    continue
+
+                f.write(create_svg_key(col_num, row_num, key_data))
+                row_num += 1
+            col_num += 1
+            row_num = 0
+        f.write("  </g>\n")
+        f.write("</svg>\n")
+
+
+
+def parse_content(infile, outfile):
+    is_reading_content = False
+    is_reading_keymaps = False
+    keymaps = []
+    for line in read_file(infile):
+        if "LAYOUTER-START" in line:
+            is_reading_content = True
+        elif "LAYOUTER-STOP" in line:
+            is_reading_content = False
+        if is_reading_content and line.startswith("KEYMAPS"):
+            is_reading_keymaps = True
+
+        if is_reading_content and line.startswith("#define"):
+            read_definitions(line)
+        if is_reading_content and is_reading_keymaps:
+            keymaps.append(line)
+
+    layouts = read_keylayout(keymaps)
+    create_svg(layouts, outfile)
+
+if len(sys.argv) == 2 and (sys.argv[1] == "-h" or sys.argv[1] == "--help" or sys.argv[1] == "-v" or sys.argv[1] == "--version"):
+    print("Keyboardio Layout Creator 1.0")
+    print("By Johan Sjöblom")
+    exit(0)
+if len(sys.argv) != 3:
+    print("Keyboardio Layout Creator usage:")
+    print(sys.argv[0] + " input_file.ino output_file.svg")
+    exit(1)
+
+parse_content(sys.argv[1], sys.argv[2])
